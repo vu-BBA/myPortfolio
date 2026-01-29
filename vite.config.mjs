abf600f65dea8c153e9732f6ae7b023b96adbdc2
vu-BBA <bushrach147hnd@gmail.com>
2026-01-29 17:08:24 +0500

    Deploy portfolio

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/portfolio2/',
+  base: "/my-portfolio/",
   plugins: [react()],
 })
